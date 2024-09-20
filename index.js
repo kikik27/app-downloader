90e003fd99cbcf615b7fa708495461c9bd070502
M.ZAKY ARISANDHI <mkikik27@gmail.com>
2024-09-20 09:10:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+// Endpoint to download APK
+app.get('/download/:filename', (req, res) => {
+  const { filename } = req.params;
+  const filePath = path.join(__dirname, 'apks', filename);
+
+  console.log('Downloading APK:', filePath);
+
+  // Check if file exists
+  if (fs.existsSync(filePath)) {
+    res.download(filePath, (err) => {
+      if (err) {
+        res.status(500).send('Error downloading file');
+      }
+    });
+  } else {
+    res.status(404).send('File not found');
+  }
+});
+
+app.get('/api/version-info', (req, res) => {
+  const responseData = {
+    "current_version": '1.11.1',
+    "app_url": "https://i.diawi.com/YzoZtx"
+  };
+
+  console.log(responseData);
+
+  res.json(responseData);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+module.exports = app;
